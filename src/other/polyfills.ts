0cf35008ed14cf4ae04d3de4c1057b1a116ee7a9
Armando Meziat <amf-commits@mezi.at>
2023-09-14 23:12:44 -0300

    Lint

--- src/other/polyfills.ts
+++ src/other/polyfills.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
